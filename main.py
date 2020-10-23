3e513e2348967548696de2762b83cd869291b935
F4bb3rs <f4bb3rs@gmail.com>
2020-10-23 12:18:18 +0200

    Bis inkl. Aufgabe 14

--- main.py
+++ main.py
@@ -12,11 +12,30 @@ def compute_frequencyTable(char_list):
 
 
 def computeMostFrequentChars(freq_table, n):
-    print(sorted(freq_table, reverse=True)[i])
     shorter = {}
     for i in range(0, n):
-        shorter[i] = sorted(freq_table, reverse=True)[i]
-        print( str(i) + " : " + str(shorter[i]))
+        shorter[i] = sorted(freq_table.items(), key=lambda x: (x[1], x[0]), reverse=True)[i]
+        #print( str(i) + " : " + str(shorter[i]))
     return shorter
 
-computeMostFrequentChars({0: 1, 4: 4, 6: 1, 7: 1, 8: 2, 11: 1, 13: 5, 14: 1, 17: 1, 18: 1,19: 2, 23: 1}, 6)
+#computeMostFrequentChars({0: 1, 4: 4, 6: 1, 7: 1, 8: 2, 11: 1, 13: 5, 14: 1, 17: 1, 18: 1,19: 2, 23: 1}, 6)
+
+
+def computeKeyPairs(char_list):
+    output = []
+    for i in char_list:
+        print("i = " + str(i))
+        char_clone = char_list[:] # Kopiere Variable, nicht nur Referenz
+        char_clone.remove(i)
+        for j in char_clone:
+            output.append((i, j))
+    print(output)
+    return output
+
+#computeKeyPairs([13,4,19])
+
+def analyzeCipherText(cipher_text, char_pairs):
+    for pair in char_pairs:
+        a = (3 * (pair[0] - pair[1])) % 26
+        b = pair[1] - 4 * a
+
