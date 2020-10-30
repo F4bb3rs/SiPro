76b1ca2bc52c4a934c01674e544d1394ec17ac63
PhilippBefeld <Phil.Befeld@gmx.de>
2020-10-30 12:24:02 +0100

    kommentare ablib

--- ablib.py
+++ ablib.py
@@ -1,44 +1,45 @@
-import aclib
+import aclib  # importiere die Bibliothek aclib
 
 def compute_frequencyTable(char_list):
-    frequency_table = {} #Dictionary anlegen
-    char_list.sort() #Eingabe aufsteigend sortieren
-    for i in char_list: # Über Eingabeliste iterienen
-        if i in frequency_table: #Wenn es den Eintrag in dem Dictionry schon gibt
-            frequency_table[i] += 1 #++
+    frequency_table = {}  # leeres Dictionary erstellen
+    char_list.sort()  # Eingabe aufsteigend sortieren
+    for i in char_list:  # Über Eingabeliste iterienen
+        if i in frequency_table:  # Wenn es den Eintrag in dem Dictionry schon gibt
+            frequency_table[i] += 1  # Anzahl für Eintrag um 1 erhöhen
         else:
-            frequency_table[i] = 1 # Eintrag anlegen
-    return frequency_table
+            frequency_table[i] = 1  # neuen Eintrag anlegen
+    return frequency_table  # gebe das Dictionary mit den enthaltenen Zahlen und ihrer Häufigkeit zurück
 
 
 
 def computeMostFrequentChars(freq_table, n):
-    shorter = []
+    shorter = []  # leere Liste erstellen
     sortedTable = sorted(freq_table.items(), key=lambda x: (x[1], x[0]), reverse=True)
-    for i in range(0, n):
-        shorter.append(sortedTable[i][0])
-    return shorter
-
-#computeMostFrequentChars({0: 1, 4: 4, 6: 1, 7: 1, 8: 2, 11: 1, 13: 5, 14: 1, 17: 1, 18: 1,19: 2, 23: 1}, 6)
+    # Dictionary wird absteigend sortiert, zuerst nach der Häufigkeit und bei gleicher Häufigkeit nach den Zahlen selbst
+    for i in range(0, n):   # Für die gewünschte Anzahl der häufigsten Zahlen
+        shorter.append(sortedTable[i][0])   # werden die Zahlen ohne ihre Häufigkeit in die Liste geschrieben
+    return shorter  # gebe die Liste mit den häufigsten Zahlen zurück
 
 
 def computeKeyPairs(char_list):
-    output = []
-    for i in char_list:
-        char_clone = char_list[:] # Kopiere Variable, nicht nur Referenz
-        char_clone.remove(i)
-        for j in char_clone:
+    output = []  # leere Liste erstellen
+    for i in char_list:  # für jede Zahl
+        char_clone = char_list[:]  # Erstelle eine neue neue zu char_list identische Liste.
+        # Kopiere Variable, nicht nur Referenz.
+        char_clone.remove(i)  # entferne aktuellen char aus der Liste, damit er nicht mit sich selbst gepaart wird
+        for j in char_clone:  # i wird mit jedem anderen char zu einem Paar kombiniert
             output.append((i, j))
-    return output
-
+    return output  # gebe die Liste mit den möglichen Keys
 
 
 def analyzeCipherText(cipher_text, char_pairs):
-    possible_plains = []
-    for pair in char_pairs:
-        a = (3 * (pair[0] - pair[1])) % 26
-        b = (pair[1] - 4 * a) % 26
-        decoded = aclib.acDecrypt(a, b, cipher_text)
-        if isinstance(decoded, str) and len(decoded) > 0:
-            possible_plains.append(decoded)
-    return possible_plains
+    possible_plains = []  # leere Liste erstellen
+    for pair in char_pairs:  # für alle Paare wir durchgeführt:
+        a = (3 * (pair[0] - pair[1])) % 26  # Berechnung von a des Keys
+        b = (pair[1] - 4 * a) % 26  # berechnung von b des Keys
+        decoded = aclib.acDecrypt(a, b, cipher_text)  # versuche das Entschlüsseln des textes mit a und b als Key und
+        # der Übergabe als Text. Zuweisung der Rückgabe zu decoded
+        if isinstance(decoded, str) and len(decoded) > 0:  # Kontrolle ob in decoded Text steht
+            possible_plains.append(decoded)  # wenn ja, neuer Eintrag in der Liste mit den möglichen Klartexten
+    return possible_plains  # gebe die Liste mit den Klartexten zurück
+
--- main.py
+++ main.py
@@ -1,37 +1,40 @@
 import aclib
 
+
 def compute_frequencyTable(char_list):
-    frequency_table = {} #Dictionary anlegen
-    char_list.sort() #Eingabe aufsteigend sortieren
-    for i in char_list: # Über Eingabeliste iterienen
-        if i in frequency_table: #Wenn es den Eintrag in dem Dictionry schon gibt
-            frequency_table[i] += 1 #++
+    frequency_table = {}  # Dictionary anlegen
+    char_list.sort()  # Eingabe aufsteigend sortieren
+    for i in char_list:  # Über Eingabeliste iterienen
+        if i in frequency_table:  # Wenn es den Eintrag in dem Dictionry schon gibt
+            frequency_table[i] += 1  # ++
         else:
-            frequency_table[i] = 1 # Eintrag anlegen
+            frequency_table[i] = 1  # Eintrag anlegen
     return frequency_table
 
-#compute_frequencyTable([4, 8, 13, 11, 0, 13, 6, 4, 17, 19, 4, 23, 19, 14, 7, 13, 4, 18,8, 13, 13])
+
+# compute_frequencyTable([4, 8, 13, 11, 0, 13, 6, 4, 17, 19, 4, 23, 19, 14, 7, 13, 4, 18,8, 13, 13])
 
 
 def computeMostFrequentChars(freq_table, n):
     shorter = []
-    sortedTable = sorted(freq_table.items(), key=lambda x: (x[1], x[0]), reverse=True)
+    sortedTable = sorted(freq_table.items(),
+                         key=lambda x: (x[1], x[0]), reverse=True)
     for i in range(0, n):
         shorter.append(sortedTable[i][0])
-    return shorter
+    return print(shorter)
 
-computeMostFrequentChars({0: 1, 4: 4, 6: 1, 7: 1, 8: 2, 11: 1, 13: 5, 14: 1, 17: 1, 18: 1,19: 2, 23: 1}, 6)
+
+computeMostFrequentChars({0: 1, 4: 4, 6: 1, 7: 1, 8: 2, 11: 1, 13: 5, 14: 1, 17: 1, 18: 1, 19: 2, 23: 1}, 6)
 
 
 def computeKeyPairs(char_list):
     output = []
     for i in char_list:
-        char_clone = char_list[:] # Kopiere Variable, nicht nur Referenz
+        char_clone = char_list[:]  # Kopiere Variable, nicht nur Referenz
         char_clone.remove(i)
         for j in char_clone:
             output.append((i, j))
     return output
-
 
 
 def analyzeCipherText(cipher_text, char_pairs):
@@ -42,10 +45,9 @@ def analyzeCipherText(cipher_text, char_pairs):
         if isinstance(decoded, str):
             print(decoded)
 
-#cipher= "MVLCDIUEGCXHDEKWSXMLUMEILCJWCVWKXVRCXPSEMVKEEKEGCXHDGMDKGMLFCSLIEKWSXKWCOJSLKXEUELKOUCSDCVLOIAKOCVKUUCSDCVLKNJIVDIVDPCLLCOHMVKUCSDCVLESXZMZKWXUJLCYXIJFUFCHDEYXKILJXCOMEKIELFKLKWFVCHCYULCJXCZMDKEKWSXMLUMVWUPKXEJIWKIOIBMVYHUKVCSYFVCHMLKXILSXKKNMELEIPCSLFCGLCMOJHKOKVLWXUJLCYXIJFUIVDFCGLCMVWCXJCXILKMLMVLCXKIHGCXHDEUELKOE"
-#char_list = aclib.decode(cipher)
-#freqTable = compute_frequencyTable(char_list)
-#mostFrequent = computeMostFrequentChars(freqTable, 10)
-#keyPairs = computeKeyPairs(mostFrequent)
-#analyzeCipherText(cipher, keyPairs)
-
+# cipher= "MVLCDIUEGCXHDEKWSXMLUMEILCJWCVWKXVRCXPSEMVKEEKEGCXHDGMDKGMLFCSLIEKWSXKWCOJSLKXEUELKOUCSDCVLOIAKOCVKUUCSDCVLKNJIVDIVDPCLLCOHMVKUCSDCVLESXZMZKWXUJLCYXIJFUFCHDEYXKILJXCOMEKIELFKLKWFVCHCYULCJXCZMDKEKWSXMLUMVWUPKXEJIWKIOIBMVYHUKVCSYFVCHMLKXILSXKKNMELEIPCSLFCGLCMOJHKOKVLWXUJLCYXIJFUIVDFCGLCMVWCXJCXILKMLMVLCXKIHGCXHDEUELKOE"
+# char_list = aclib.decode(cipher)
+# freqTable = compute_frequencyTable(char_list)
+# mostFrequent = computeMostFrequentChars(freqTable, 10)
+# keyPairs = computeKeyPairs(mostFrequent)
+# analyzeCipherText(cipher, keyPairs)
