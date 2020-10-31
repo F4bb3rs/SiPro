10934b2058e23cf9f98d4e8a752c1ec0d26e39b3
F4bb3rs <f4bb3rs@gmail.com>
2020-10-31 11:08:58 +0100

    analyCipherText fix & Umstellung auf ASCII Encoding

--- ablib.py
+++ ablib.py
@@ -4,49 +4,51 @@ import aclib  # importiere die Bibliothek aclib
 def computeFrequencyTable(char_list):
     frequency_table = {}  # Leeres Dictionary erstellen
     char_list.sort()  # Eingabe aufsteigend sortieren
-    for i in char_list:  # Über Eingabeliste iterienen
+    for i in char_list:  # ?ber Eingabeliste iterienen
         if i in frequency_table:  # Wenn es den Eintrag in dem Dictionry schon gibt,
-            frequency_table[i] += 1  # wird die Anzahl für den Eintrag um 1 erhöht
+            frequency_table[i] += 1  # wird die Anzahl f?r den Eintrag um 1 erh?ht
         else:
             frequency_table[i] = 1  # Ansonsten wir ein neuer Eintrag erstellt
-    return frequency_table  # Gebe das Dictionary mit den enthaltenen Zahlen und ihrer Häufigkeit zurück
+    return frequency_table  # Gebe das Dictionary mit den enthaltenen Zahlen und ihrer H?ufigkeit zur?ck
 
 
 def printFrequencyTable(freq_table):
-    for x in freq_table: #für alle Schlüssel
-        print(chr(x+97), " : ", freq_table[x])#Ausgabe von Schlüssel als Kleinbuchstabe mit zugehörigem Wert
+    for x in freq_table: #f?r alle Schl?ssel
+        print(chr(x+97), " : ", freq_table[x])#Ausgabe von Schl?ssel als Kleinbuchstabe mit zugeh?rigem Wert
 
 
 def computeMostFrequentChars(freq_table, n):
     mostFrequent = []  # Leere Liste erstellen
+    print(freq_table)
     sortedTable = sorted(freq_table.items(), key=lambda x: (x[1], x[0]), reverse=True)  # Dictionary wird absteigend
-    # sortiert, zuerst nach der Häufigkeit und bei gleicher Häufigkeit nach den Zahlen selbst
-    for i in range(0, n):  # Für die gewünschte Anzahl der häufigsten Zahlen,
-        mostFrequent.append(sortedTable[i][0])   # werden die Zahlen ohne ihre Häufigkeit in die Liste geschrieben
-    return mostFrequent  # Gebe die Liste mit den häufigsten Zahlen zurück
+    print(sortedTable)
+    # sortiert, zuerst nach der H?ufigkeit und bei gleicher H?ufigkeit nach den Zahlen selbst
+    for i in range(0, n):  # F?r die gew?nschte Anzahl der h?ufigsten Zahlen,
+        mostFrequent.append(sortedTable[i][0])   # werden die Zahlen ohne ihre H?ufigkeit in die Liste geschrieben
+    return mostFrequent  # Gebe die Liste mit den h?ufigsten Zahlen zur?ck
 
 
 def computeKeyPairs(char_list):
-    keyPairs = []  # Leere Liste für die Zahlenpaare erstellen
-    for i in char_list:  # Für jede Zahl:
+    keyPairs = []  # Leere Liste f?r die Zahlenpaare erstellen
+    for i in char_list:  # F?r jede Zahl:
         char_clone = char_list[:]  # Erstelle eine neue zu char_list identische Liste.
         char_clone.remove(i)  # Entferne aktuellen char aus der Liste, damit er nicht mit sich selbst gepaart wird
         for j in char_clone:  # i wird mit jedem anderen char zu einem Paar kombiniert
             keyPairs.append((i, j))
-    return keyPairs  # Gebe die Liste mit den möglichen Keys
+    return keyPairs  # Gebe die Liste mit den m?glichen Keys
 
 
 
 
 
 def analyzeCipherText(cipher_text, char_pairs):
-    possible_plains = []  # Leere Liste für mögliche Klartexte erstellen
-    for pair in char_pairs:  # Für alle Paare wir durchgeführt:
-        a = (3 * (pair[0] - pair[1])) % 26  # Berechnung von a des Keys
-        b = (pair[1] - 4 * a) % 26  # Berechnung von b des Keys
-        decoded = aclib.acDecrypt(a, b, cipher_text)  # Versuche das Entschlüsseln des textes mit a und b als Key und
-        # der Übergabe als Text. Zuweisung der Rückgabe zu decoded
+    possible_plains = []  # Leere Liste f?r m?gliche Klartexte erstellen
+    for pair in char_pairs:  # F?r alle Paare wir durchgef?hrt:
+        a = (3 * (pair[1] - pair[0])) % 26  # Berechnung von a des Keys
+        b = (pair[0] - 4 * a) % 26  # Berechnung von b des Keys
+        decoded = aclib.acDecrypt(a, b, cipher_text)  # Versuche das Entschl?sseln des textes mit a und b als Key und
+        # der ?bergabe als Text. Zuweisung der R?ckgabe zu decoded
         if isinstance(decoded, str) and len(decoded) > 0:  # Kontrolle ob in decoded Text steht
-            possible_plains.append(decoded)  # ,wenn ja wird ein neuer Eintrag in der Liste mit den möglichen Klartexten
+            possible_plains.append(decoded)  # ,wenn ja wird ein neuer Eintrag in der Liste mit den m?glichen Klartexten
             # erzeugt
-    return possible_plains  # Gebe die Liste mit den Klartexten zurück
+    return possible_plains  # Gebe die Liste mit den Klartexten zur?ck
--- affinebreaker.py
+++ affinebreaker.py
@@ -12,22 +12,22 @@ if not path.endswith(".txt"):  # Test auf Dateityp
     print("Dateityp Fehler")
     exit(2)
 try:
-    file = open(path, "r")  # Versuch die txt-Datei zu öffnen
+    file = open(path, "r")  # Versuch die txt-Datei zu ?ffnen
     cipher = file.read()
-except IOError:  # Ausgabe von Fehler falls nicht möglich
+except IOError:  # Ausgabe von Fehler falls nicht m?glich
     print("Datei Fehler")
     exit(3)
 
-# Aufruf aller benötigten Funktionen zum Entschlüsseln. Funktionen geben als Rückgabe die Übergabeparameter für die
-# nächste Funktion aus
+# Aufruf aller ben?tigten Funktionen zum Entschl?sseln. Funktionen geben als R?ckgabe die ?bergabeparameter f?r die
+# n?chste Funktion aus
 char_list = aclib.decode(cipher)    # Decodieren von Buchstaben zu Zahlen
-freqTable = ablib.computeFrequencyTable(char_list)  # Erstellen der Häufigkeitstabelle
-mostFrequent = ablib.computeMostFrequentChars(freqTable, 10)  # Finden der n Häufigsten Zahlen
+freqTable = ablib.computeFrequencyTable(char_list)  # Erstellen der H?ufigkeitstabelle
+mostFrequent = ablib.computeMostFrequentChars(freqTable, 6)  # Finden der n H?ufigsten Zahlen
 keyPairs = ablib.computeKeyPairs(mostFrequent)  # Erstellen der Zahlenpaare
-broken_text = ablib.analyzeCipherText(cipher, keyPairs)  # Finden aller möglichen Klartexte unter Verwendung der
+broken_text = ablib.analyzeCipherText(cipher, keyPairs)  # Finden aller m?glichen Klartexte unter Verwendung der
 # Zahlenpaare und des Geheimtextes
 
 print("Found " + str(len(broken_text)) + " possible Plaintexts")  # Ausgabe der Anzahl der gefundenen Texte
 for i in broken_text:
     i = (i[:50]) if len(i) > 50 else i
-    print(i)  # Ausgabe der ersten 50 Zeichen jedes möglichen entschlüsselten Textes
+    print(i)  # Ausgabe der ersten 50 Zeichen jedes m?glichen entschl?sselten Textes
